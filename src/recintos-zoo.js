00777615a41134d7024c0e69573276709e13dde0
Isabeli Romano Rodrigues <isabeli.romano@gmail.com>
2024-09-12 17:02:22 -0300

    Implementação de verificação booleana em animais carnívoros

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -9,13 +9,14 @@ class RecintosZoo {
             { especie: 'hipopotamo', tamanho: 4, bioma: ['savana', 'rio'] },
         ];
 
-        this.recintos = [
-            { numero: 1, bioma: 'savana', tamanho: 10, animais: [{ especie: 'MACACO', quantidade: 3 }] },
-            { numero: 2, bioma: 'floresta', tamanho: 5, animais: [] },
-            { numero: 3, bioma: 'savana e rio', tamanho: 7, animais: [{ especie: 'GAZELA', quantidade: 1 }] },
-            { numero: 4, bioma: 'rio', tamanho: 8, animais: [] },
-            { numero: 5, bioma: 'savana', tamanho: 9, animais: [{ especie: 'LEAO', quantidade: 1 }] }
-        ];
+        this.animais = [
+            { especie: "leao", tamanho: 3, biomas: ["savana"], carnivoro: true },
+            { especie: "leopardo", tamanho: 2, biomas: ["savana"], carnivoro: true },
+            { especie: "crocodilo", tamanho: 3, biomas: ["rio"], carnivoro: true },
+            { especie: "macaco", tamanho: 1, biomas: ["savana", "floresta"], carnivoro: false },
+            { especie: "gazela", tamanho: 2, biomas: ["savana"], carnivoro: false },
+            { especie: "hipopotamo", tamanho: 4, biomas: ["savana", "rio"], carnivoro: false },
+          ];
     }
     analisaRecintos(animal, quantidade) {
     }
